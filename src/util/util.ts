a53ef7d4db96c010942ecb7fb7469abf134fbaea
phanhoaian0608 <33730739+phanhoaian0608@users.noreply.github.com>
2023-11-09 22:44:07 +0700

    run local

---
+++ src/util/util.ts
@@ -0,0 +1,54 @@
+import fs from "fs";
+import Jimp = require("jimp");
+
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const photo = await Jimp.read(inputURL);
+      const outpath =
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      await photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname + outpath, (img) => {
+          resolve(__dirname + outpath);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
+}
+
+export async function getFilePathsFromDirectory(dir: string) {
+  const files = fs.readdirSync(dir).map(file => dir + "/" + file)
+  return files
+}
+
+export function isValidUrl(str) {
+  const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
